737166ce47fa2cd5ddaa579cc35f08b00c548c7f
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2026-03-12 13:45:14 +0000

    feat: projeto finalizado!

--- TelaLoginCadastro/src/app/pages/home/home.ts
+++ TelaLoginCadastro/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+  constructor(private router: Router){}
+
+  logout(): void{
+    localStorage.removeItem('usuarioLogado');
+    this.router.navigate(['/']);
+  }
+
+}
